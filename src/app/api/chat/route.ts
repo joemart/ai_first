8bd7d787452afb150993824d28ee7fd0149e0112
Joseph Martire <joemart06@gmail.com>
2024-11-23 14:16:34 -0400

    Add functioning 2 tools; add functioning toolInvocation in UI

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,15 +1,43 @@
 import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
+import { streamText, tool, jsonSchema } from 'ai';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
+
+
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const doubleSchema = jsonSchema<{math:number}>({
+    type: 'object',
+    properties: {
+      math: {
+        type:"number"
+      }
+    },
+    required:["math"]
+  })
+
   const result = streamText({
     model: openai('gpt-4'),
-    messages,
+
+    tools:{
+      double:tool({
+        description:"Return the number doubled",
+        parameters:doubleSchema,
+        execute: async({math})=>`Your return number => ${math*2}`
+      }),
+
+      triple:tool({
+        description:"Return the number tripled",
+        parameters:doubleSchema,
+        execute: async({math})=>`Your return number => ${math*3}`
+      }),
+    },
+    
+  // prompt:"Get potato"
+  messages
   });
 
   return result.toDataStreamResponse();
